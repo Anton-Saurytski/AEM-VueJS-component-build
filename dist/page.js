a6239034d5bbf5e35617087ae0d9a48a4a135f22
Byron Ponce <byron@polished-web.com>
2017-03-16 15:24:31 -0700

    use "page" instead of "root"

---
+++ dist/page.js
@@ -0,0 +1,17 @@
+var aemVuePage = (function () {
+'use strict';
+
+var page = {render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{attrs:{"id":"aem_vue_page"}},[_c('component-a',{attrs:{"node":_vm.componentA}}),_c('component-b',{attrs:{"node":_vm.componentB}})],1)},staticRenderFns: [],
+  el: '#root',
+  data: function data$1(){
+    return data
+  },
+  components: {
+    componentA: componentA,
+    componentB: componentB
+  }
+};
+
+return page;
+
+}());
