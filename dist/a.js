68a46b68614f250df47c599e583ad7815a0c208a
Byron Ponce <byron@polished-web.com>
2017-03-16 15:13:07 -0700

    add working example

---
+++ dist/a.js
@@ -0,0 +1,12 @@
+var componentA = (function () {
+'use strict';
+
+var a = {render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"componentA"},[_c('h1',[_vm._v(_vm._s(_vm.node.text))])])},staticRenderFns: [],
+  props: [
+    'node'
+  ]
+};
+
+return a;
+
+}());
